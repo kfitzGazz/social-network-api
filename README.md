71418d5acf4c7028fe02010cc4bf89e43a49d89f
Kyleigh Fitzgerald <kyleigh.fitzgerald22@gmail.com>
2023-12-13 21:27:57 -0500

    save everything without video recording

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# social-network-api
-Built an API for a social network web app where users can share their thoughts, react to friends' thoughts, and create a friend list. 
+# Social Network API
+
+## Description
+
+Created a social network API with CRUD and testing code in insomnia
+
+## Installation
+
+Reveal code in file explorer, open in VScode, open terminal, npm i, npm start
+
+## Usage
+
+Here is a video on how to use:
+
+## Credits
+
+University of Bootcamp activities for starter code and Phil for walking us through models, controllers, APIs and index.js
+
+## License
+
+MIT
---
+++ models/User.js
@@ -0,0 +1,56 @@
+const { Schema, model } = require('mongoose');
+
+// Schema to create User model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/]
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought',
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+// Create a virtual property `fullName` that gets and sets the user's full name
+userSchema
+  .virtual('fullName')
+  // Getter
+  .get(function () {
+    return `${this.first} ${this.last}`;
+  })
+  // Setter to set the first and last name
+  .set(function (v) {
+    const first = v.split(' ')[0];
+    const last = v.split(' ')[1];
+    this.set({ first, last });
+  });
+
+// Initialize our User model
+const User = model('user', userSchema);
+
+module.exports = User;
